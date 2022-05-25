fafbbdf49533d6d33c334f4a303078609047c907
rafaelmedeiross <rafael.medeiros@msn.com>
2022-05-25 13:28:23 -0300

    correção final final

--- desafio10.sql
+++ desafio10.sql
@@ -1,5 +1,4 @@
-SELECT song.song_name AS nome, 
-COUNT(history.song_id) AS reproducoes
+SELECT song.song_name AS nome, COUNT(history.song_id) AS reproducoes
 FROM SpotifyClone.history as history
 JOIN SpotifyClone.songs AS song ON history.song_id = song.song_id
 JOIN SpotifyClone.users AS users ON users.user_id = history.user_id
