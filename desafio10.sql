0b45c0aabbb298c7796244d0bcb9d48aca1793ba
rafaelmedeiross <rafael.medeiros@msn.com>
2022-05-25 12:56:46 -0300

    correção

--- desafio10.sql
+++ desafio10.sql
@@ -3,5 +3,5 @@ COUNT(history.song_id) AS reproducoes
 FROM SpotifyClone.history as history
 INNER JOIN SpotifyClone.songs AS song ON history.song_id = song.song_id
 INNER JOIN SpotifyClone.users AS users ON users.user_id = history.user_id
-WHERE users.plan_type BETWEEN 'Gratuito' AND 'Pessoal';
+WHERE users.plan_id BETWEEN 1 AND 4
 GROUP BY nome;
